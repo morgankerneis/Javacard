a921135ae367a958fb28a5ef92705c6c1f141594
Morgan <morgan.kerneis@gmail.com>
2025-06-17 14:14:39 +0200

    Json Serializer operational

---
+++ src/main/java/fr/afpa/ContactJsonSerializer.java
@@ -0,0 +1,36 @@
+package fr.afpa;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+public class ContactJsonSerializer implements Serializer<Contact> {
+    @Override
+    public void saveList(String filePath, List<Contact> objectsToSave) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            File file = new File(filePath);
+            mapper.writeValue(file, objectsToSave);
+        } catch (Exception e) {
+            System.err.println("An error occurred while saving the file.");
+        }
+    }
+
+    public void exportJsonToFile(Stage stage, List<Contact> contacts) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save JSON File");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("JSON Files", "*.json"));
+
+        File selectedFile = fileChooser.showSaveDialog(stage);
+        if (selectedFile != null) {
+            saveList(selectedFile.getAbsolutePath(), contacts);
+        }
+    }
+}
